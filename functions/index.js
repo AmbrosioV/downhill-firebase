916c10473c8a77084bc85aeec7572b6007d9a1f5
ambrosiov <aovaldes@uc.cl>
2020-11-04 19:09:08 -0300

    add coins

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,34 @@
+'use-strict';
+
 const functions = require('firebase-functions');
-var admin = require("firebase-admin");
+const admin = require("firebase-admin");
+const express = require('express');
 
-var serviceAccount = require("downhill-dash-firebase-adminsdk.json");
+const serviceAccount = require("./downhill-dash-firebase-adminsdk.json");
 
-var app = admin.initializeApp({
+admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://downhill-dash.firebaseio.com"
 });
 
+const firestore = admin.firestore();
+const app = express();
 
-exports.hello = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+app.get('/addcoins/:user/:coins', (req, res) => {
+  console.log(req.params);
+  console.log(req.params.user);
+  console.log(req.params.coins);
+  firestore.collection('users').doc(req.params.user)
+    .update({coins: admin.firestore.FieldValue.increment(parseInt(req.params.coins))})
+    .then(doc => {
+      return res.send(doc.data());
+    }).catch(err => {
+      return res.status(500).send(err);
+    })
 });
+
+app.get('/hi', (req, res) => {
+  return res.send("HIHO");
+});
+
+exports.app = functions.https.onRequest(app);
