21d9f9f1aa328c5a9928b26cf0022c5324ec3e8e
ambrosiov <aovaldes@uc.cl>
2020-11-03 22:41:37 -0300

    admin credentials

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,12 @@
 const functions = require('firebase-functions');
+var admin = require("firebase-admin");
+
+var serviceAccount = require("downhill-dash-firebase-adminsdk.json");
+
+var app = admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://downhill-dash.firebaseio.com"
+});
 
 
 exports.hello = functions.https.onRequest((request, response) => {
