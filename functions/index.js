07209fd2a0b7e12c44a9b23bebcf86e3df04ac9a
ambrosiov <aovaldes@uc.cl>
2020-11-11 21:13:11 -0300

    fix(login): new achievements for userInitParams

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-"use-strict";
+  "use-strict";
 
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
@@ -23,6 +23,8 @@ const userInitParams = {
     firstPlace: false,
     rich: false,
     secret: false,
+    login: false, 
+    addFriend: false,
   },
   coins: 20,
   daily: admin.firestore.FieldValue.serverTimestamp(),
